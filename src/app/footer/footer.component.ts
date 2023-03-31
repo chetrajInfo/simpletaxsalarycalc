282df5148c193b5ac18c7b330beb036b5e19d1c1
myinfo <chetraj0101@gmail.com>
2023-03-31 00:51:02 -0500

    added new compoennt called fontawesome for icon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{ faLinkedin, faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit{
+   myfacebookIcon = faFacebook;
+   mylinkedIcon = faLinkedin;
+   mytwitterIcon = faTwitter;
+   myinstagramIcon = faInstagram;
+   
+      constructor(){}
+      ngOnInit(): void {
+        
+      }
 }
